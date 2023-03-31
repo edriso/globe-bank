b2c1d1a1c72fb28859b3918fbf835f376d37933e
edriso <edriso.dev@gmail.com>
2023-04-01 00:32:48 +0200

    add page title

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -1,12 +1,22 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
-<?php include_once(SHARED_PATH . '/staff_header.php'); ?>
+<?php
+$page_title = 'Show Page';
+include_once(SHARED_PATH . '/staff_header.php');
+?>
 
 <?php
 
 $id = $_GET['id'] ?? '1';
-echo h($id);
 
 ?>
+
+<div id="content">
+    <a href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+
+    <div class="page show mt-2">
+        <h3>Page ID: <?php echo h($id); ?></h3>
+    </div>
+</div>
 
 <?php include_once(SHARED_PATH . '/staff_footer.php'); ?>
