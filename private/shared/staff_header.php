8b972824bece2949d659006eb1d2856314209b69
edriso <edriso.dev@gmail.com>
2023-04-01 00:35:45 +0200

    update page

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>GBI - <?php echo $page_title ;?></title>
+    <title>GBI - <?php echo h($page_title) ;?></title>
 </head>
 
 <body>
--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,13 +1,22 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
-<?php include_once(SHARED_PATH . '/staff_header.php'); ?>
+<?php
+$page_title = 'Show Subject';
+include_once(SHARED_PATH . '/staff_header.php');
+?>
 
 <?php
 
 $id = $_GET['id'] ?? '1';
 
-echo h($id);
-
 ?>
+
+<div id="content">
+    <a href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
+
+    <div class="subject show mt-2">
+        <h3>Subject ID: <?php echo h($id); ?></h3>
+    </div>
+</div>
 
 <?php include_once(SHARED_PATH . '/staff_footer.php'); ?>
