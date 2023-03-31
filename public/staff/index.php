6b1c87d3292a5540c47fcaa60c1dfb42340c3b2e
edriso <edriso.dev@gmail.com>
2023-03-31 16:47:18 +0200

    add defined paths

--- public/staff/index.php
+++ public/staff/index.php
@@ -1,9 +1,8 @@
 <?php require_once('../../private/initialize.php'); ?>
 
 <?php $page_title = "Homepage"; ?>
-<?php include_once('../../private/shared/staff_header.php'); ?>
-
+<?php include_once(SHARED_PATH . '/staff_header.php'); ?>
 
 <div>content</div>
 
-<?php include_once('../../private/shared/staff_footer.php'); ?>
+<?php include_once(SHARED_PATH . '/staff_footer.php'); ?>
---
+++ public/staff/subjects/index.php
@@ -0,0 +1,8 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<?php $page_title = "Subjects"; ?>
+<?php include_once(SHARED_PATH . '/staff_header.php'); ?>
+
+<div>content</div>
+
+<?php include_once(SHARED_PATH . '/staff_footer.php'); ?>
