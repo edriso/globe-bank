ff1850920df5cac7d94dec044b6f440c59aa5a47
edriso <edriso.dev@gmail.com>
2023-04-02 21:35:19 +0200

    add stylesheet

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo url_for('/css/style.css')?>">
     <title>GBI - <?php echo h($page_title) ;?></title>
 </head>
 
